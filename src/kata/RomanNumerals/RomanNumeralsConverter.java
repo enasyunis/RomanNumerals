7852a825f0662f4dc3b3b2e4f1f2d6f0bf3961d3
Enas Yunis <enasyunis@hotmail.com>
2016-04-05 18:44:21 -0400

    Pass Testing for 9

--- src/kata/RomanNumerals/RomanNumeralsConverter.java
+++ src/kata/RomanNumerals/RomanNumeralsConverter.java
@@ -9,10 +9,13 @@ public class RomanNumeralsConverter {
             if (leftOver >= 10) {
                 sb.append("X");
                 leftOver -= 10;
+            } else if (leftOver >= 9) { // == would have been sufficient - but will not over think it - per TDD
+                sb.append("IX");
+                leftOver -= 9;
             } else if (leftOver >= 5) {
                 sb.append("V");
                 leftOver -= 5;
-            } else if (leftOver >= 4) {
+            } else if (leftOver >= 4) { // == would have been sufficient - but will not over think it - per TDD
                 sb.append("IV");
                 leftOver -= 4;
             } else if (leftOver >= 1) {
--- test/RomanNumeralsConverterTest.java
+++ test/RomanNumeralsConverterTest.java
@@ -7,14 +7,15 @@ public class RomanNumeralsConverterTest {
     @Test
     public void WhenRomanNumeralsConverterIsPassedANumberItReturnsANumeral(){
         RomanNumeralsConverter converter = new RomanNumeralsConverter();
+
         assertEquals("Passing 1", "I", converter.toRomanNumeral(1));
         assertEquals("Passing 2", "II", converter.toRomanNumeral(2));
 
         assertEquals("Passing 5", "V", converter.toRomanNumeral(5));
         assertEquals("Passing 4","IV", converter.toRomanNumeral(4));
 
-
         assertEquals("Passing 10","X", converter.toRomanNumeral(10));
+        assertEquals("Passing 9","IX", converter.toRomanNumeral(9));
 
 
     }
