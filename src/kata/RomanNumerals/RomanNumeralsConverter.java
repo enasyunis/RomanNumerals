ad31100b0cdf46ca85fa1a2c356b43d1db17fd08
Enas Yunis <enasyunis@hotmail.com>
2016-04-05 18:35:12 -0400

    Passing Testing for 1, 2, and 5

--- src/kata/RomanNumerals/RomanNumeralsConverter.java
+++ src/kata/RomanNumerals/RomanNumeralsConverter.java
@@ -4,9 +4,13 @@ package kata.RomanNumerals;
 public class RomanNumeralsConverter {
     public String toRomanNumeral(Integer arabicNumber) {
         StringBuilder sb = new StringBuilder();
-        int leftOver = arabicNumber.intValue();
+        int leftOver = arabicNumber;
         while (leftOver > 0) {
-            if (leftOver >= 1) {
+            if (leftOver >= 5) {
+                sb.append("V");
+                leftOver-=5;
+            }
+            else if (leftOver >= 1) {
                 sb.append("I");
                 leftOver--;
             }
--- test/RomanNumeralsConverterTest.java
+++ test/RomanNumeralsConverterTest.java
@@ -7,8 +7,11 @@ public class RomanNumeralsConverterTest {
     @Test
     public void WhenRomanNumeralsConverterIsPassedANumberItReturnsANumeral(){
         RomanNumeralsConverter converter = new RomanNumeralsConverter();
-        assertEquals(converter.toRomanNumeral(1), "I");
-        assertEquals(converter.toRomanNumeral(2), "II");
+        assertEquals("Passing 1", converter.toRomanNumeral(1), "I");
+        assertEquals("Passing 2", converter.toRomanNumeral(2), "II");
+
+        assertEquals("Passing 5", converter.toRomanNumeral(5), "V");
 
     }
+
 }
